4b46fab9345c7ae2f9e2944a6973afd59e523a7e
magdalena krasicka <krasickamagdalena@gmail.com>
2023-10-16 15:42:01 +0100

    Generated models and set the relitionship between them, created seed file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  # created parent comment association
+  belongs_to :parent_comment, class_name: "Comment", optional:true
+  # replied which are the child comments
+  has_many :replies, class_name: "Comment", foreign_key: :parent_comment_id, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Post.destroy_all
+
+post = Post.create(title: "First Post", content: "This is my first post's content.")
+
+parent_comment = Comment.create(commentary: "This is a parent comment.", post: post)
+
+child_comment1 = Comment.create(commentary: "This is a reply to the parent comment.", parent_comment: root_comment, post: post)
+child_comment2 = Comment.create(commentary: "This is another reply to the parent comment.", parent_comment: root_comment, post: post)
+
+reply_to_child1 = Comment.create(commentary: "This is a reply to the first child comment.", parent_comment: child_comment1, post: post)
